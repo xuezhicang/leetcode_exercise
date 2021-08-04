cf107b7841d9409a6b50d0ca6fc1c1d70b4cf13a
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-08-04 10:59:45 -0500

    Create leetcode_94_Binary_Tree_Inorder_Traversal.java

---
+++ leetcode_94_Binary_Tree_Inorder_Traversal/leetcode_94_Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public void inorder(TreeNode root,List ans){
+        if(root == null)
+            return;
+        inorder(root.left,ans);
+        ans.add(root.val);
+        inorder(root.right,ans);          
+    }
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        List ans = new ArrayList<>();       
+        inorder(root,ans);
+        return ans;      
+    }
+}
