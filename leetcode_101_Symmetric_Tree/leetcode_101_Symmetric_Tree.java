3d8adfa865c1aafada7292a84f2e1f41aa88da45
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-08-09 15:35:20 -0500

    Create leetcode_101_Symmetric_Tree.java

---
+++ leetcode_101_Symmetric_Tree/leetcode_101_Symmetric_Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public boolean helper(TreeNode r1,TreeNode r2){
+        
+        if(r1 == null && r2 ==null ) return true;
+        if(r1 == null || r2 ==null ) return false;
+        if(r1.val != r2.val) return false;
+        return helper(r1.left,r2.right) && helper(r1.right,r2.left);
+    } 
+
+    
+    public boolean isSymmetric(TreeNode root) {
+        
+      
+        return helper(root,root);
+        
+
+        
+        
+    }
+}
