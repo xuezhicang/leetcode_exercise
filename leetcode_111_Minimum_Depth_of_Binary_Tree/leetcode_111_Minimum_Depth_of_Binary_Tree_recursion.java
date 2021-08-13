a6914b2525f8849200be69bdce0016bf933c6b83
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-08-13 18:38:42 -0500

    Create leetcode_111_Minimum_Depth_of_Binary_Tree_recursion.java

---
+++ leetcode_111_Minimum_Depth_of_Binary_Tree/leetcode_111_Minimum_Depth_of_Binary_Tree_recursion.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        int res = Integer.MAX_VALUE;
+        
+        if(root.left != null)
+            res = Math.min(res,minDepth(root.left)+1);
+        if(root.right != null)
+            res = Math.min(res,minDepth(root.right)+1);
+        
+        
+        return res;
+        
+    }
+}
