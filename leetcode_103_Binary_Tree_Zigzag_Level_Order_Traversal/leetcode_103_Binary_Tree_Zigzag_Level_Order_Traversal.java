b0f9aa902afe83680806231bb9ea435961da9d89
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-08-18 14:32:31 -0500

    Create leetcode_103_Binary_Tree_Zigzag_Level_Order_Traversal.java

---
+++ leetcode_103_Binary_Tree_Zigzag_Level_Order_Traversal/leetcode_103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void helper(TreeNode root,List<List<Integer>> res,int level){
+        
+        if(root == null) return;
+        
+        if(res.size() <= level)
+            res.add(new ArrayList<Integer>());
+        res.get(level).add(root.val);        
+            
+
+        helper(root.left,res,level+1);
+        helper(root.right,res,level+1);
+        
+
+            
+        
+
+        return;
+        
+        
+        
+    }
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null) return res;
+        helper(root,res,0);
+
+        for(int i =1;i<res.size();i = i+2)
+           Collections.reverse(res.get(i));
+        
+        return res;
+            
+    }
+}
