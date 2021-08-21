17c91a9759b558e9e37683c76aab26dd2275ca0f
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-08-20 23:09:55 -0500

    Create leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree_recursion.java

---
+++ leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree/leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree_recursion.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    TreeNode res = null;
+    
+    public boolean helper(TreeNode root, TreeNode p, TreeNode q){
+        
+        if(root == null) return false;
+        
+        int left = helper(root.left, p, q)?1:0;
+        int right = helper(root.right, p, q)?1:0;
+        
+        int mid= (root.val ==p.val ||root.val==q.val)?1:0;
+        
+        if(left+right+mid>=2)
+            res = root;
+        
+            
+        return (left+right+mid>0);
+    }
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        helper(root, p, q);
+        
+        return res;
+        
+    }
+}
