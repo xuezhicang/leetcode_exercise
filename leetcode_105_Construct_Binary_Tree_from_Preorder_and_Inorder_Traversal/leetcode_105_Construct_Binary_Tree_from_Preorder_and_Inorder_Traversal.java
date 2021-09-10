28b7b542adabf5f628645e2d6e43bc1d269bbdca
Xuezhi Cang <10249824+xuezhicang@users.noreply.github.com>
2021-09-10 14:55:46 -0500

    Create leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java

---
+++ leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/leetcode_105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,43 @@
+class Solution {
+    int preorder_s = 0;
+    
+    public int findIndex(int[] array, int num){
+        int idx = -1;
+        
+        int i =0;
+        while(idx == -1){
+            if(array[i] == num){
+                idx = i;
+                
+            }
+            i++;
+        }
+        
+        return idx;
+    }
+    
+    public TreeNode helper(int[] preorder, int[] inorder,int inorder_s,int inorder_e){
+        
+        if(inorder_s > inorder_e) return null;
+        
+        int nnn = preorder[preorder_s++];
+        TreeNode root = new TreeNode(nnn);
+        
+        
+
+        int index = findIndex(inorder, nnn);  
+        
+        root.left = helper(preorder,inorder,inorder_s,index-1);
+        root.right = helper(preorder,inorder,index+1,inorder_e);
+        
+        return root;
+        
+        
+        
+    }
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        return helper(preorder,inorder,0,inorder.length-1);
+    }
+}
